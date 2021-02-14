8465fb997e581a0761bac37d32895641e3266cf0
AjayLiu <ajayliu2928@gmail.com>
2021-02-13 18:06:37 -0800

    change google analytics link

--- src/components/GoogleAnalyticsHook.tsx
+++ src/components/GoogleAnalyticsHook.tsx
@@ -4,7 +4,7 @@ import ReactGA from 'react-ga'
 const GoogleAnalyticsHook: React.FC = () => {
 
     useEffect(()=>{
-        ReactGA.initialize('UA-178410803-1')
+        ReactGA.initialize('UA-178410803-7')
         ReactGA.set({ page: window.location.pathname })
         ReactGA.pageview(window.location.pathname)
     }, [])
